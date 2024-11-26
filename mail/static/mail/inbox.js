21da4b9d1fe3e30a7233ecbb5f4e6f94da47c0eb
pritamumaiya <pritamumaiya@gmail.com>
2024-11-26 04:19:36 +0000

    adding send email

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -8,6 +8,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
+
+  // Send email
+  document.querySelector('#compose-form').addEventListener('submit', (e) => {
+    // Prevent the form form being submmited
+    e.preventDefault();
+
+    send_email(
+      document.querySelector('#compose-recipients').value,
+      document.querySelector('#compose-subject').value,
+      document.querySelector('#compose-body').value
+    );
+  });
+
 });
 
 function compose_email() {
@@ -30,4 +43,25 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email(recipients,subject, body) {
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      if (result['message']) {
+        load_mailbox('sent');
+        alert(result['message']);
+      } else {
+        alert(result['error']);
+      }
+  });
 }
